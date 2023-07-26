00f10674798fbf74717792fbcdf151d490abd06a
pradumyabanke <pradumgurjar2@gmail.com>
2023-07-26 19:06:21 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css"
+import Layout from "./layouts/Layout";
+
+function App() {
+    return <Layout />;
+}
+
+export default App;
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,25 @@
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Contact from '../pages/Contact'
+import Singup from '../pages/Singup'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorsDetails from '../pages/Doctors/DoctorDetails'
+
+import { Routes, Route } from 'react-router-dom'
+const Routers = () => {
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/doctors" element={<Doctors />} />
+    <Route path="/doctors/:id" element={<DoctorsDetails />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Singup />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/services" element={<Services />} />
+
+  </Routes>
+
+}
+
+export default Routers
